331d49fa6f11e71fcefda6549970ceefa403b8d8
Matt <44646040+mm3011@users.noreply.github.com>
2025-05-10 19:16:48 -0600

    Imported FastRestart Project

---
+++ FastRestart/Patches/SubmitChatPatch.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+namespace FastRestart.Patches
+{
+    [HarmonyPatch(typeof(HUDManager), "SubmitChat_performed")]
+    internal class SubmitChatPatch
+    {
+        private static bool Prefix(HUDManager _instance, ref InputAction.CallbackContext context)
+        {
+            if (!context.performed) return true;
+            if (string.IsNullOrEmpty(_instance.chatTextField.text)) return true;
+
+            PlayerControllerB local = GameNetworkManager.Instance.localPlayerController;
+            if (local == null) return true;
+
+            StartOfRound manager = local.playersManager;
+            if(manager == null) return true;
+
+            string text = _instance.chatTextField.text;
+
+            if (FastRestartBase.verifying)
+            {
+                if (text.ToLower() == "y" || text.ToLower() == "yes")
+                {
+                    ResetTextbox(_instance, local);
+                    if (!local.isInHangarShipRoom || !manager.inShipPhase || manager.travellingToNewLevel)
+                    {
+                        FastRestartBase.SendChatMessage("Cannot restart, ship must be in orbit.");
+                        return false;
+                    }
+                    FastRestartBase.AcceptRestart(manager);
+                    return false;
+                }
+                if (text.ToLower() == "n" || text.ToLower() == "no")
+                {
+                    ResetTextbox(_instance, local);
+                    FastRestartBase.DeclineRestart();
+                    return false;
+                }
+                return true;
+            }
+            if(text.ToLower() == "/restart" || text.ToLower() == "/reset")
+            {
+                ResetTextbox(_instance, local);
+                if(!GameNetworkManager.Instance.isHostingGame)
+                {
+                    FastRestartBase.SendChatMessage("Must be lobby host to reset game.");
+                    return false;
+                }
+
+                if (FastRestartBase.bypassConfirm) FastRestartBase.AcceptRestart(manager);
+                else FastRestartBase.ConfirmRestart();
+
+                return false;
+            }
+            return true;
+        }
+
+        private static void ResetTextbox(HUDManager manager, PlayerControllerB local)
+        {
+            local.isTypingChat = false;
+            manager.chatTextField.text = "";
+            EventSystem.current.SetSelectedGameObject(null);
+            manager.PingHUDElement(manager.Chat, 2f, 1f, 0.2f);
+            manager.typingIndicator.enabled = false;
+        }
+    }
+}
---
+++ FastRestart/Plugin.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace FastRestart
+{   
+    [BepInPlugin(modGUID,modName,modVersion)]
+    public class FastRestartBase : BaseUnityPlugin
+    {
+        //Mod Info
+        private const string modGUID = "kos.fastRestart";
+        private const string modName = "Fast Restart";
+        private const string modVersion = "1.0.0";
+
+        public static bool verifying = false;
+        private ConfigEntry<bool> overrideConfirmation;
+        public static bool bypassConfirm = false;
+
+        private Harmony harmony;
+        private static MethodInfo chat;
+
+        private void Awake()
+        {
+            overrideConfirmation = Config.Bind("Config", "Override Confirmation", false, "Ignore the confirmation step of restarting.");
+            bypassConfirm = overrideConfirmation.Value;
+
+            harmony = new Harmony(modGUID);
+            harmony.PatchAll();
+
+            chat = AccessTools.Method(typeof(HUDManager), "AddChatMessage");
+
+            Logger.LogInfo($"{modGUID} loaded!");
+        }
+
+        public static void SendChatMessage(string message)
+        {
+            chat?.Invoke(HUDManager.Instance, new object[] { message, "" });
+            HUDManager.Instance.lastChatMessage = "";
+        }
+
+        public static void ConfirmRestart()
+        {
+            verifying = true;
+            SendChatMessage("Are you sure? Y/N.");
+        }
+
+        public static void AcceptRestart(StartOfRound manager)
+        {
+            SendChatMessage("Restart confirmed.");
+            verifying = false;
+
+            int[] stats = new int[]
+            {
+                manager.gameStats.daysSpent,
+                manager.gameStats.scrapValueCollected,
+                manager.gameStats.deaths,
+                manager.gameStats.allStepsTaken
+            };
+            manager.FirePlayersAfterDeadlineClientRpc(stats);
+        }
+
+        public static void DeclineRestart()
+        {
+            SendChatMessage("Restart aborted.");
+            verifying = false;
+        }
+    }
+}
